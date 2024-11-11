170e0694383ccc93c3493b1ce3c07687d1631afd
Ketan9548 <kc918062@gmail.com>
2024-11-11 10:34:44 +0530

    done

--- Backend/DBConnection/Connection.js
+++ Backend/DBConnection/Connection.js
@@ -1,11 +1,15 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const DB_Connection = 'mongodb+srv://LibraryK123:Kapil12@cluster0.wn8zk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const DB_Connection =
+  "mongodb+srv://LibraryK123:Kapil12@cluster0.wn8zk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const connected = mongoose.connect(DB_Connection)
-.then(()=>{
-    console.log("DB is Connected")
-})
-
+const connected = mongoose
+  .connect(DB_Connection)
+  .then(() => {
+    console.log("DB is Connected");
+  })
+  .catch((err) => {
+    console.log("error in Database connection: ", err);
+  });
 
 export default connected;
